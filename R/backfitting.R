099292770e65f1fd847d4e1e6d943ef6557deeea
stephens999 <stephens999@gmail.com>
2016-03-06 09:36:18 -0600

    tidy backfitting to work with matrices

--- R/backfitting.R
+++ R/backfitting.R
@@ -1,14 +1,14 @@
-#' backfitting for correction K factors model
+#' backfitting to correct K-factor model
 #'
-#' correction the factor and loading matrix estimation using backfitting algorithm
+#' Correct the factor and loading matrix estimates using backfitting algorithm
 #'
 #' @param Y is the data matrix (N by P)
-#' @param Lest is estimation for l to correct
-#' @param Fest is estimation for f to correct
-#' @param tautol this the stop criterion for convergence, number for iteration
-#' @param numtau number for iteration in flash rank one.
+#' @param Lest is estimate for l to correct
+#' @param Fest is estimate for f to correct
+#' @param maxiter_bf maximum number of iterations
+#' @param maxiter_r1 maximum number of iterations in flash rank one.
 #'
-#' @details greedy algorithm on the residual matrix to get a rank one matrix decomposition
+#' @details Repeatedly applies rank 1 algorithm to Y-L[,-i]F[,-i]'
 #'
 #' @export backfitting
 #'
@@ -16,61 +16,51 @@
 #'
 #' @return list of factor, loading and variance of noise matrix
 #'  \itemize{
-#'   \item{\code{l}} {is a K by N matrix for loadings}
-#'   \item{\code{f}} {is a K by P matrix for factors}
+#'   \item{\code{l}} {is a N by K matrix for loadings}
+#'   \item{\code{f}} {is a P by K matrix for factors}
 #'  }
 #' @examples
 #' N = 100
 #' P = 200
 #' Y = matrix(rnorm(N*P,0,1),ncol=P)
-#' L = matrix(rnorm(N*2,0,1),ncol=2)
-#' F = matrix(rnorm(P*2,0,1),ncol=2)
 #' g = greedy(Y,K = 10)
 #' gb = backfitting(Y,g$l,g$f,maxiter_bf=100,maxiter_r1 = 5)
 #'
-
-# tautol is the number of iterations here
 backfitting = function(Y,Lest,Fest,maxiter_bf=100,maxiter_r1 = 500){
   # backfitting with initial values
   epsilon = 1
   tau = 1
+  if(is.vector(Lest)){Lest = matrix(Lest,ncol=1)}
+  if(is.vector(Fest)){Fest = matrix(Fest,ncol=1)}
+  if(nrow(Lest)!=nrow(Y)){stop("Lest of wrong dimension for Y")}
+  if(nrow(Fest)!=ncol(Y)){stop("Fest of wrong dimension for Y")}
+
   while(epsilon>1e-5 & tau < maxiter_bf){
     tau = tau + 1
-    # if the l or f is a vector
-    if(is.vector(Lest) || is.vector(Fest)){
-      residual = Y - Lest %*% t(Fest)
-      preRMSfl = sqrt(mean((Lest %*% t(Fest))^2))
-      residual = residual + Lest %*% t(Fest)
+    K = dim(Lest)[2]
+    if(K==0){break} #tests for case where all factors disappear!
+    # this one can be put out of the while loop
+    residual = Y - Lest %*% t(Fest)
+    preRMSfl = sqrt(mean((Lest %*% t(Fest))^2))
+    for(i in 1:K){
+      residual = residual + Lest[,i] %*% t(Fest[,i])
       r_flash = flash_r1(residual,maxiter_r1 = maxiter_r1)
-      Lest = r_flash$l
-      Fest = r_flash$f
-      residual = residual - Lest %*% t(Fest)
-    }else{
-      K = dim(Lest)[2]
-      # this one can be put out of the while loop
-      residual = Y - Lest %*% t(Fest)
-      preRMSfl = sqrt(mean((Lest %*% t(Fest))^2))
-      for(i in 1:K){
-        residual = residual + Lest[,i] %*% t(Fest[,i])
-        r_flash = flash_r1(residual,maxiter_r1 = maxiter_r1)
-        Lest[,i] = r_flash$l
-        Fest[,i] = r_flash$f
-        residual = residual - Lest[,i] %*% t(Fest[,i])
-      }
-      # remove the zero in the l and f
-      while(i <= dim(Lest)[2] ){
-        if(sum((Lest[,i])^2)==0 || sum((Fest[,i])^2)==0){
-          Lest = Lest[,-i]
-          Fest = Fest[,-i]
-        }
-        numfactor = ifelse(is.vector(Lest),1,dim(Lest)[2])
-        if(numfactor == 1){
-          break
-        }
-        i = i+1
-      }
+      Lest[,i] = r_flash$l
+      Fest[,i] = r_flash$f
+      residual = residual - Lest[,i] %*% t(Fest[,i])
     }
-
+    # remove the zero in the l and f
+    while(i <= dim(Lest)[2] ){
+      if(all(Lest[,i]==0) || all(Fest[,i]==0)){
+        Lest = Lest[,-i,drop=FALSE]
+        Fest = Fest[,-i,drop=FALSE]
+      }
+      numfactor = dim(Lest)[2]
+      if(numfactor == 1 || numfactor==0){
+        break
+      }
+      i = i+1
+    }
     RMSfl = sqrt(mean((Lest %*% t(Fest))^2))
     epsilon = abs(preRMSfl - RMSfl)
   }
---
+++ tests/testthat/test_backfitting.R
@@ -0,0 +1,17 @@
+test_that("backfitting produces same results for vector as matrix", {
+  set.seed(10)
+  l = rnorm(10)
+  f=rnorm(5)
+  Y = l %*% t(f) + rnorm(10*5)
+  temp=backfitting(Y,l,f)
+  temp2 = backfitting(Y,matrix(l,ncol=1),matrix(f,ncol=1))
+  expect_equal(temp,temp2)
+})
+
+test_that("backfitting works when results goes to 0", {
+  set.seed(10)
+  l = rnorm(10)
+  f=rnorm(5)
+  Y = matrix(rnorm(10*5),nrow=10)
+  expect_silent(temp=backfitting(Y,l,f))
+})
