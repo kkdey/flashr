7745d41e8189204ce08231574945e82716796342
Kushal K Dey <kshldey@gmail.com>
2016-08-23 20:06:01 -0500

    added data

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' FLASH Factor analysis model fit for Deng et al 2014 single cell RNA-seq data
+#' on mouse
+#' @format A list of model fit (loadings and factors) for k=10
+#' @return A list of model fit (loadings and factors) for k=10
+"flash_deng"
